4cebe5c84c2265cf18f927c1ba9f110d647d778b
quihuang <quihuang2017@gmail.com>
2020-09-22 07:57:11 -0500

    Diseño Login Borrador

---
+++ src/controller/EjercicioEjemplo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+/**
+ *
+ * @author quihu
+ */
+public class EjercicioEjemplo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int n= 0, s= 0;
+       
+       do{
+      
+           System.out.println(s);
+           
+       }while (s<10);
+    }
+    
+}
